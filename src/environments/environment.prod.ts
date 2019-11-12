1c6d48856197cdaeca06e961ea719b29c4006181
Paul Liu <paullby@gmail.com>
2019-11-12 18:27:54 -0500

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
             network: Btc.networks.bitcoin
         },
         ETH: {
-            chain: 'mainnet', 
+            chain: 'mainnet',
             hardfork: 'petersburg',
             web3Provider: 'https://mainnet.infura.io/v3/6c5bdfe73ef54bbab0accf87a6b4b0ef'
         },
@@ -14,25 +14,25 @@ export const environment = {
             chain: {
                 name: 'mainnet',
                 networkId: 0,
-                chainId: 0                
+                chainId: 0
             }
         }
-    },    
+    },
     endpoints: {
         blockchaingate: 'https://blockchaingate.com/v2/',
         coingecko: 'https://api.coingecko.com/',
         kanban: 'https://kanbanprod.fabcoinapi.com/',
-        ankit: 'http://18.223.17.4:3002/',
+        ankit: 'http://fabprod.fabcoinapi.com:3002/',
         BTC: {
             exchangily: 'https://btcprod.fabcoinapi.com/'
         },
         FAB: {
             exchangily: 'https://fabprod.fabcoinapi.com/'
-        },                
+        },
         ETH: {
             exchangily: 'https://ethprod.fabcoinapi.com/',
         },
-        pricehistory: 'http://18.223.17.4:3002/klinedata/'        
+        pricehistory: 'https://fabprod.fabcoinapi.com:3002/klinedata/'
     },
     CoinType: {
         BTC: 0,
@@ -52,7 +52,7 @@ export const environment = {
             { name: 'BTC', address: '1CKg6irbGXHxBHuTx7MeqYQUuMZ8aEok8z'},
             {name: 'ETH', address: '0xe7721493eea554b122dfd2c6243ef1c6f2fe0a06'},
             {name: 'USDT', address: '0xe7721493eea554b122dfd2c6243ef1c6f2fe0a06'}
-        ],        
+        ],
     },
     websockets: {
         allprices: 'wss://fabprod.fabcoinapi.com:3002/ws/allprices',
