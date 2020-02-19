85e9a8e0b318e50eb0184a994b3d36203634cc91
keninqiu <jack.li@fa.biz>
2020-02-19 10:18:08 -0500

    fix

--- src/app/modules/otc/modals/otc-place-order/otc-place-order.ts
+++ src/app/modules/otc/modals/otc-place-order/otc-place-order.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, EventEmitter, Output } from '@angular/core';
 import {  ModalDirective } from 'ngx-bootstrap/modal';
 import { faAlipay } from '@fortawesome/free-brands-svg-icons';
 import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'otc-place-order-modal',
@@ -11,9 +12,14 @@ import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
 export class OtcPlaceOrderModal {
     faAlipay = faAlipay;
     faCreditCard = faCreditCard;
-
+    otcPlaceOrderForm = this.fb.group({
+        quantity: ['0'],
+        amount: ['0'],
+        method: ['alipay']
+    });
     @ViewChild('otcPlaceOrderModal', {static: true}) public otcPlaceOrderModal: ModalDirective;
     methods: string[] = ['alipay', 'bank'];
+    constructor(private fb: FormBuilder) { }
     show() {
         this.otcPlaceOrderModal.show();
     }
@@ -21,4 +27,7 @@ export class OtcPlaceOrderModal {
     hide() {
         this.otcPlaceOrderModal.hide();
     }    
+    onSubmit() {
+        
+    }
 }
