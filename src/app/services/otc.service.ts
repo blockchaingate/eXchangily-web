084a5a538fcf16f1bd819fcb027345ef4dde289c
Paul Liu <paullby@gmail.com>
2020-03-11 22:42:24 -0400

    temp

---
+++ src/app/services/otc.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { price_list, coin_list } from '../config/coins';
+
+@Injectable()
+export class OtcService {
+    constructor(private http: HttpClient) {
+    }
+
+    getPriceList() {
+        return price_list;
+    }
+
+    getCoinList() {
+        return coin_list;
+    }
+}
