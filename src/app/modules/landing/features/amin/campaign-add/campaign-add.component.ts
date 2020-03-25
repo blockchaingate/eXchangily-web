625afb22e34bc1ffb55a849a60f30c12296f3b9d
ken <kenin.qiu@gmail.com>
2020-03-25 16:44:10 -0400

    fix css

--- src/app/modules/landing/features/amin/campaign-add/campaign-add.component.ts
+++ src/app/modules/landing/features/amin/campaign-add/campaign-add.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-campaign-add',
     templateUrl: './campaign-add.component.html',
-    styleUrls: ['./campaign-add.component.css']
+    styleUrls: ['./campaign-add.component.scss']
   })
   export class CampaignAddComponent implements OnInit {
     ngOnInit() {
