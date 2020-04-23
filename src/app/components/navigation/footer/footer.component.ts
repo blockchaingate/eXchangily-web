ce17ae24c15e21d4ffe9cfe14605da11ecc0d9fb
Paul Liu <bitspadorg@gmail.com>
2020-04-23 13:09:06 -0400

    social media

--- src/app/components/navigation/footer/footer.component.ts
+++ src/app/components/navigation/footer/footer.component.ts
@@ -6,25 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  imgWechatShow: boolean;
   year: number;
   constructor() { }
 
   ngOnInit() {
-    this.imgWechatShow = false;
     this.year = (new Date()).getFullYear();
   }
 
-  coordinates(event: MouseEvent): void {
-    const clientX = event.offsetX;
-    const clientY = event.offsetY;
-    // console.log('clientX=' + clientX + ',clientY=' + clientY);
-    if (clientX >= 0 && clientY >= 0) {
-        if (clientX <= 26 && clientY <= 26) {
-            this.imgWechatShow = true;
-        } else {
-            this.imgWechatShow = false;
-        }
-    }
-  }  
 }
