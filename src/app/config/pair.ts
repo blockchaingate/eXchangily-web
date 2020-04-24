a4657fc8c30c09bcfdcf21d7aa431f828da2512d
Paul Liu <bitspadorg@gmail.com>
2020-04-23 22:14:58 -0400

    update

---
+++ src/app/config/pair.ts
@@ -0,0 +1,13 @@
+import {Pair} from '../modules/market/models/pair';
+
+export const pair_list: Pair[] = [
+    { name: 'BTC_USDT', base: 'USDT', priceDecimal: 2, qtyDecimal: 4 },
+    { name: 'ETH_USDT', base: 'USDT', priceDecimal: 2, qtyDecimal: 4 },
+    { name: 'FAB_USDT', base: 'USDT', priceDecimal: 2, qtyDecimal: 4 },
+    { name: 'EXG_USDT', base: 'USDT', priceDecimal: 2, qtyDecimal: 4 },
+
+    { name: 'BTC_DUSD', base: 'DUSD', priceDecimal: 2, qtyDecimal: 4 },
+    { name: 'ETH_DUSD', base: 'DUSD', priceDecimal: 2, qtyDecimal: 4 },
+    { name: 'FAB_DUSD', base: 'DUSD', priceDecimal: 2, qtyDecimal: 4 },
+    { name: 'EXG_DUSD', base: 'DUSD', priceDecimal: 2, qtyDecimal: 4 },
+];
