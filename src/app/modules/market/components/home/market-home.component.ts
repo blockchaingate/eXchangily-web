27eb1be4abbd120f5e055744f03dc856f159fa25
Paul Liu <bitspadorg@gmail.com>
2020-06-22 20:17:35 -0400

    update

--- src/app/modules/market/components/home/market-home.component.ts
+++ src/app/modules/market/components/home/market-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
+
 @Component({
   selector: 'app-market-home',
   templateUrl: './market-home.component.html',
