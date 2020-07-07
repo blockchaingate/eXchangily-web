0e0b5ecc95654a9c328915f561aa59efb4219e6b
ken <kenin.qiu@gmail.com>
2020-07-07 14:58:44 -0400

    add new ui

---
+++ src/app/modulesnew/wallet/components/faq/faq.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+selector: 'app-new-wallet-faq',
+templateUrl: './faq.component.html',
+styleUrls: ['./faq.component.scss']
+})
+export class FaqComponent {
+constructor() {
+    }
+}
---
+++ src/app/modulesnew/wallet/components/overview/overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+selector: 'app-new-wallet-overview',
+templateUrl: './overview.component.html',
+styleUrls: ['./overview.component.scss']
+})
+export class OverviewComponent {
+constructor() {
+    }
+}
