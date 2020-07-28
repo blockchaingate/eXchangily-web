294314dc177c2371a370ed2006eebf7b9df7654a
ken <kenin.qiu@gmail.com>
2020-07-28 11:11:37 -0400

    update version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,7 @@ export const environment = {
     baseUrl: 'https://exchangily.com',
     url: 'https://kanbanprod.fabcoinapi.com/',
     tmpUrl: 'https://kanbanprod.fabcoinapi.com/', // replace this url once the temp api is deployed    
-    version: '1.2.6',
+    version: '1.2.7',
     campaignId: 1,
     OTC_COMMISSION_RATE: 0.01,
     chains: {
