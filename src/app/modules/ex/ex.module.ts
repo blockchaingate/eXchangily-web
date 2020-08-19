032699290db54dab74d1cac0df37445b29d52bd5
ken <kenin.qiu@gmail.com>
2020-08-19 13:47:46 -0400

    fix

--- src/app/modules/ex/ex.module.ts
+++ src/app/modules/ex/ex.module.ts
@@ -5,7 +5,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { ExRoutingModule } from './ex-routing.module';
 import { CodeComponent } from './components/code/code.component';
-
+import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
 import { UtilService } from '../../services/util.service';
 import { CoinService } from '../../services/coin.service';
@@ -19,6 +19,7 @@ import { AlertService } from '../../services/alert.service';
         CommonModule,
         ExRoutingModule,
         TranslateModule,
+        FormsModule,
         CommonModule,
         SharedModule,
         ModalModule.forRoot()
