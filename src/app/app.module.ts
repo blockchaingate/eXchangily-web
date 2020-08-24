e54fcffb3791328f9cb047ab25a41c438aa517be
Paul Liu <bitspadorg@gmail.com>
2020-08-24 11:31:22 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule  } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -57,6 +58,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatToolbarModule,
     MatIconModule,
     MatFormFieldModule,
+    MatSelectModule,
     MatMenuModule,
     MatProgressSpinnerModule,
     MatListModule,
--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./help.component.css']
 })
 export class HelpComponent {
+    ticketCats: [];
+    
     constructor() {}
 }
