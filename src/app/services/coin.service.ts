1e46f8ed71cf9fb58f660d1a3e9975d881031ac6
Paul Liu <bitspadorg@gmail.com>
2020-12-02 14:54:25 -0500

    update

--- src/app/services/coin.service.ts
+++ src/app/services/coin.service.ts
@@ -3,9 +3,9 @@ import { MyCoin } from '../models/mycoin';
 import * as BIP32 from 'node_modules/bip32';
 import * as Btc from 'bitcoinjs-lib';
 import * as bitcoinMessage from 'bitcoinjs-message';
-import { hdkey } from 'ethereumjs-wallet/dist';
 import * as bchaddr from 'bchaddrjs';
-// import { hdkey } from 'ethereumjs-wallet'; deposit not working
+// import { hdkey } from 'ethereumjs-wallet/dist'; deposit not working
+import * as hdkey from 'ethereumjs-wallet';
 
 import { Address } from '../models/address';
 import { coin_list } from '../config/coins';
