24e28a1c6a374ac44204133750408ee5d8f4f1a7
Paul Liu <bitspadorg@gmail.com>
2020-12-02 14:51:39 -0500

    update

--- src/app/components/help/announcement/announcement.module.ts
+++ src/app/components/help/announcement/announcement.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { AnnouncementComponent } from './announcement/announcement.component';
-// import { AnnouncementListComponent } from './announcement-list/announcement-list.component';
+import { AnnouncementComponent } from './announcement/announcement.component';
+import { AnnouncementListComponent } from './announcement-list/announcement-list.component';
 
 @NgModule({
   declarations: [
