59d56c40d8fc22a0c4a314a80cd789c4e42357af
Paul Liu <bitspadorg@gmail.com>
2020-12-18 18:17:15 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
     baseUrl: 'https://exchangily.com',
     url: 'https://kanbanprod.fabcoinapi.com/',
     tmpUrl: 'https://kanbanprod.fabcoinapi.com/', // replace this url once the temp api is deployed    
-    version: '1.5.1',
+    version: '2.0.0',
     campaignId: 1,
     OTC_COMMISSION_RATE: 0.01,
     chains: {
