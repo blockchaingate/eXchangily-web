b8881ef8c2928674df8e36a8c028893089de59cc
Paul Liu <bitspadorg@gmail.com>
2021-01-04 20:45:37 -0500

    update

--- src/app/modules/smartcontract/smartcontract.module.ts
+++ src/app/modules/smartcontract/smartcontract.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SmartContractComponent } from './pages/smart-contract/smart-contract.component';
 import { SmartContractRoutingModule } from './smartcontract-routing.module';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { StorageService } from '../../services/storage.service';
@@ -13,7 +13,8 @@ import { Web3Service } from '../../services/web3.service';
 import { UtilService } from '../../services/util.service';
 import { CoinService } from '../../services/coin.service';
 import { AlertService } from '../../services/alert.service';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import {MatTabsModule} from '@angular/material/tabs';
     FormsModule,
     MatTabsModule,
     ReactiveFormsModule,
+    TranslateModule,
     SharedModule
   ],
   providers: [
