845cac916f8ce2d2c2212b3e2cb742e85950e858
ken <kenin.qiu@gmail.com>
2021-03-04 10:24:38 -0500

    added order detail

--- src/app/modules/kanbanexplorer/kanbanexplorer-routing.module.ts
+++ src/app/modules/kanbanexplorer/kanbanexplorer-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlockDetailsComponent } from './components/block-details/block-details.component';
-import { BlocksComponent } from './components/blocks/blocks.component';
+import { OrdeDetailComponent } from './components/order-detail/order-detail.component';
 import { TxDetailsComponent } from './components/tx-details/tx-details.component';
 import { AddressDetailsComponent } from './components/address-details/address-details.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'block-detail/:blockNumber', component: BlockDetailsComponent },
   { path: 'tx-detail/:txhash', component: TxDetailsComponent },
+  { path: 'order-detail/:orderHash', component: OrdeDetailComponent },
   { path: 'address-detail/:address', component: AddressDetailsComponent }
 ];
 
