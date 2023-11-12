1168ad56e200c08aa99c48237e0882f33b136830
Paul Liu <paullby@gmail.com>
2023-11-12 00:09:16 -0500

    update

--- build-prod.sh
+++ build-prod.sh
@@ -1,3 +1,4 @@
 #node patch  if some error, run node patch after npm i
 #ng build --prod --aot=true --optimization=true
-ng build --configuration production --aot 
+#ng build --configuration production --aot 
+ng build --configuration=production --aot=true --optimization=true
