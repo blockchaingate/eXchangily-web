8074bc8a69f5e5b72609d70083ed02457d84b3dc
ken <kenin.qiu@gmail.com>
2024-06-13 15:19:15 -0400

    update orders

--- src/app/modules/transaction-history/trades/trades.component.ts
+++ src/app/modules/transaction-history/trades/trades.component.ts
@@ -3,7 +3,6 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ApiService } from 'src/app/services/api.service';
 import { TradeElement } from '../trade-element';
-
 @Component({
   selector: 'app-trades',
   templateUrl: './trades.component.html',
@@ -20,29 +19,20 @@ export class TradesComponent implements OnInit {
     'orderHashOne', 
     'orderHashTwo', 
     'pair', 
-    'oneGet', 
-    'oneGiveFee', 
-    'awardBackToTwo', 
-    'twoGet', 
-    'twoGiveFee', 
-    'awardBackToOne', 
     'filledStatus', 
-    'timestamp', 
-    'txid'
+    'timestamp'
   ];
   dataSource: any = new MatTableDataSource<TradeElement>([]);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngOnInit(): void {
-
-    /*
     this.apiServ.getExchangilyExchangeTrades(this.pageSize, this.pageIndex).subscribe(
       (orders: any) => {
         this.dataSource.data = orders;
       }
     );
-    */
+
     this.apiServ.getExchangilyExchangeTradesTotalCount().subscribe(
       (totalCount: any) => {
         this.length = totalCount;
